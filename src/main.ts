547c6443dce44ea8d90b407772dd5d3d1881c055
Dmitriy <deims99@mail.ru>
2024-08-25 17:28:42 +0300

    before review

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('port') || 3000;
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.enableCors();
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.use(helmet());
+
+  await app.listen(port);
 }
 bootstrap();
